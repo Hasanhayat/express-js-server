dede8a2f932c6eea872ac17378e0683818fbfaff
hasanhayat <aylskhsmvz.v@gmail.com>
2025-04-28 23:13:43 +0500

    server

--- server.mjs
+++ server.mjs
@@ -10,7 +10,12 @@ app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
 app.get("/api/data", (req, res) => {
-  res.json({ message: "Hello from the API!" });
+  res.json({
+    message: "Hello from the API!",
+    data: [1, 2, 3, 4, 5],
+    status: "success",
+    text: "text from server",
+  });
 });
 
 app.listen(PORT, () => {
